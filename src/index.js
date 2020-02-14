9e004f32162f73b328eb22aa0154f15d275abb95
Ron Norasak <ronnorasak@gmail.com>
2020-02-13 20:40:22 -0600

    added map to project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 console.log('test');
+
+const mapboxgl = require('mapbox-gl');
+
+mapboxgl.accessToken =
+  'pk.eyJ1Ijoicm5vcmFzYWsiLCJhIjoiY2s2bGs1MGl0MGR2dTNkbzducmNicnVmaCJ9.aLeE6n_Ri7RUQ65fbRgFsA';
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  center: [-87.6354, 41.8885], //FSA Chicago
+  zoom: 12, // starting zoom
+  style: 'mapbox://styles/mapbox/streets-v10', // mapbox has lots of different map styles available.
+});
